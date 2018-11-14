5b3bc0dbdc2724191864b21a97925efe0bed35da
Josiah Green <JosiahGreen90@gmail.com>
2018-11-14 10:53:04 -0800

    Part 1 + 2 finished

---
+++ operators/operators.swift
@@ -0,0 +1,53 @@
+let a = 1 + 2
+-a
+// (returns negative)
+
+// Ternary Operators
+
+var ternary = true
+
+var string1 = "string1 gets returned"
+
+var string2 = "string2 gets returned"
+
+ternary ? string 1 : string2
+
+var d = 8
+var e = 6
+
+d = e
+
+// Template Literal
+
+"Hello" + " World"
+
+// Modulu
+
+9 % 4 
+
+// Minus unary operator
+var c = 1 
+
+let minusOne = -c
+
+// If statement
+
+let name = "John"
+
+if name == "John" {
+  print("This statement is true")
+} else {
+  print("This statement is false")
+}
+
+// nil coalescing operator
+
+let gpsDirections = "north"
+var paperMapDirections: String?
+
+var navigate = paperMapDirections ?? gpsDirections
+
+// Range Operators
+
+
+
