8130f0bd8968b505915a8cab38e5c52169f15548
Josiah Green <JosiahGreen90@gmail.com>
2018-11-14 10:39:43 -0800

    variables

---
+++ helloWorld/helloWorld.swift
@@ -0,0 +1,19 @@
+import: UIKit
+
+let maximumNumberOfWithdrawals = 5
+
+var withdrawalAttemps = 3
+
+var a = 2, b = 0, c = 1
+let d = 2.2, e = 1.1, f = 3.3
+
+var helloSwift3 String
+
+
+var north, south, easy, west: String
+
+var someString = "This is a type inference"
+
+print(someString)
+
+print("What's an example of type inference? \(someString)")
